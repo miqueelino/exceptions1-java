3ba4b5b92117861fade17aadb754a7626bb5766f
miqueelino <joao.jnunes@mercadolivre.com>
2025-07-23 13:49:52 -0300

    Primeiro commit

---
+++ src/App.java
@@ -0,0 +1,37 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import model.entities.Reservation;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy");
+
+
+        System.out.print(" Enter room number : ");
+        int number = sc.nextInt();
+        System.out.print(" Check-in date (dd/mm/yyyy): ");
+        Date checkin = sdf.parse(sc.next());
+        System.out.print(" Check-out date (dd/mm/yyyy): ");
+        Date checkout = sdf.parse(sc.next());
+        
+        if(!checkout.after(checkin)){
+            System.out.println("Error in reservation: chck-out date must be after check-in date ");
+        }else{
+            Reservation reservation = new Reservation( number, checkin, checkout);
+            System.out.println("Reservation: " + reservation);
+        }
+
+
+
+
+
+
+        sc.close();
+
+
+    }
+}
